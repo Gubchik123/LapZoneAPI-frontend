a0a004b0fb3e83109d65ed84aa4625f870132f2c
Nikita <nikita.gubarev31@gmail.com>
2023-08-21 11:54:24 +0300

    Add route for the ErrorView
    Add RegEx route for all other routes with redirect to error page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import FAQsView from "@/views/FAQsView.vue";
 import AboutView from "@/views/AboutView.vue";
+import ErrorView from "@/views/ErrorView.vue";
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,6 +18,16 @@ const router = createRouter({
             name: "about",
             component: AboutView
         },
+		{
+			path: "/error",
+			name: "error",
+			component: ErrorView,
+		},
+        // All other routes
+		{
+			path: "/:pathMatch(.*)*",
+			redirect: "/error",
+		},
     ],
 });
 
