0d3b46c7829dff5bc2e0ebf8b7a1fa188c977b2b
Nikita <nikita.gubarev31@gmail.com>
2023-08-21 11:52:01 +0300

    Create views/FAQsView.vue

---
+++ src/views/FAQsView.vue
@@ -0,0 +1,17 @@
+<template>
+	<BaseLayout title="FAQs" content_title="FAQs page">
+		<template #advanced_content>
+            <Alert category="primary" message="Soon" />
+        </template>
+	</BaseLayout>
+</template>
+
+<script>
+import BaseLayout from "@/layouts/BaseLayout.vue";
+import Alert from "@/components/Alert.vue";
+
+export default {
+    name: "FAQsView",
+    components: { BaseLayout, Alert }
+};
+</script>
