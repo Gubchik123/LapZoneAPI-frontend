80be4bcdbfcee48d770e077303c5874815192943
Nikita <nikita.gubarev31@gmail.com>
2023-08-21 11:48:38 +0300

    Create layouts/BaseLayout.vue with the Header component

---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,42 @@
+<template>
+	<Header />
+
+	<main>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="my-3 content-between">
+					<h2>{{ content_title }}</h2>
+
+                    <slot name="to_right_of_content_title"></slot>
+				</div>
+			</div>
+			<slot name="advanced_content">
+                <div
+                    class="row rounded px-3"
+                    :class="{'mb-5': is_content_mb_5}"
+                >
+                    <div class="panel-bg-color col-12 rounded p-3">
+                        <slot></slot>
+                    </div>
+                </div>
+            </slot>
+		</div>
+	</main>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+	name: "BaseLayout",
+	components: { Header },
+	props: {
+		title: { type: String, required: true },
+		content_title: { type: String, required: true },
+        is_content_mb_5: { type: Boolean, default: true }
+	},
+	mounted() {
+		document.title = `${this.title} | LapZoneAPI`;
+	},
+};
+</script>
