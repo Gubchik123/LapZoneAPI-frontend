5d25c255f379d8346afa8d2f914b12accb6e6536
Nikita <nikita.gubarev31@gmail.com>
2023-08-21 11:48:18 +0300

    Update the App.vue
    Add the RouterView, GoToTopButton and
    Footer components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
-<template></template>
+<template>
+    <div class="wrapper d-flex flex-column min-vh-100">
+        <!-- Header component in the layouts/BaseLayout.vue -->
+        <router-view />
+        <go-to-top-button />
+        <Footer />
+    </div>
+</template>
 
 <script>
+import GoToTopButton from "./components/buttons/GoToTopButton.vue";
+import Footer from "./components/Footer.vue";
+
 export default {
 	name: "App",
+    components: { GoToTopButton, Footer }
 };
 </script>
