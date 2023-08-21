f514ca52353dffe427a6300841e2b1aa2e9103ca
Nikita <nikita.gubarev31@gmail.com>
2023-08-21 11:44:54 +0300

    Create components/forms/SearchForm.vue

---
+++ src/components/forms/SearchForm.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="search_form d-flex justify-content-center">
+		<form
+			role="search"
+			action="/products/"
+			class="search d-flex align-items-center"
+		>
+			<input
+				:value="$route.query.q || ''"
+				name="q"
+				type="search"
+				aria-label="Title..."
+				placeholder="Title..."
+				class="form-control search me-2 ms-lg-3 ms-sm-0"
+			/>
+			<button type="submit" class="btn btn-success mt-1">Search</button>
+		</form>
+	</div>
+</template>
+
+<style scoped>
+@media screen and (max-width: 576px) {
+	/* Bootstrap5 .sm breakpoint */
+	.search_form {
+		margin-bottom: 20px;
+	}
+}
+</style>
