bca1a18a70b8966c276e1a69db8db82f81cf02e1
Nikita <nikita.gubarev31@gmail.com>
2023-08-23 17:36:33 +0300

    Add route for the SignUpView

--- src/router/auth_routes.js
+++ src/router/auth_routes.js
@@ -1,4 +1,5 @@
 import SignInView from "@/views/auth/SignInView.vue";
+import SignUpView from "@/views/auth/SignUpView.vue";
 
 function reject_if_user_is_authenticated(to, from, next) {
     if (localStorage.getItem("token")) {
@@ -11,6 +12,12 @@ export default [
         path: "signin",
         name: "signin",
         component: SignInView,
+		beforeEnter: [reject_if_user_is_authenticated],
+    },
+    {
+        path: "signup",
+        name: "signup",
+        component: SignUpView,
 		beforeEnter: [reject_if_user_is_authenticated],
     },
 ]
