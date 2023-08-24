1e8b209c95af1a493b387b4091812359078db612
Nikita <nikita.gubarev31@gmail.com>
2023-08-24 15:41:09 +0300

    Create views/profile/PasswordUpdateView.vue
    Add new user api funtion for updating user password

--- src/api/user.js
+++ src/api/user.js
@@ -1,5 +1,16 @@
 import { get_json_headers, get_auth_headers } from "./api";
 
+export function update_user_password(new_password, current_password, server_url) {
+    return fetch(`${server_url}/auth/users/set_password/`, {
+        method: "POST",
+        headers: { ...get_json_headers(), ...get_auth_headers() },
+        body: JSON.stringify({ current_password, new_password }),
+    }).then((response) => {
+        if (!response.ok) throw new Error();
+        return response;
+    });
+}
+
 export function delete_user(current_password, server_url) {
     return fetch(`${server_url}/auth/users/me/`, {
         method: "DELETE",
---
+++ src/views/profile/PasswordUpdateView.vue
@@ -0,0 +1,67 @@
+<template>
+	<ProfileLayout title="Customer profile">
+		<div class="col-lg-9 col-md-12 px-0">
+			<div id="content" class="panel-bg-color p-3 rounded">
+				<form @submit.prevent="update">
+					<!-- Password field -->
+					<p>
+						<label for="password">New password:</label>
+						<input
+							v-model.lazy.trim="new_password"
+							@focus="confirmation = true"
+							required=""
+							id="password"
+							type="password"
+							class="form-control"
+							placeholder="Password"
+						/>
+					</p>
+					<ConfirmPasswordModal
+						v-show="confirmation"
+						@confirm="update"
+						:error_message="error_message"
+					/>
+				</form>
+			</div>
+		</div>
+	</ProfileLayout>
+</template>
+
+<script>
+import BackendMixin from "@/mixins/BackendMixin.js";
+import ProfileLayout from "@/layouts/ProfileLayout.vue";
+import ConfirmPasswordModal from "@/components/profile/ConfirmPasswordModal.vue";
+
+import { update_user_password } from "@/api/user.js";
+
+export default {
+	name: "UpdatePasswordView",
+	mixins: [BackendMixin],
+	components: { ProfileLayout, ConfirmPasswordModal },
+	data() {
+		return { new_password: "", confirmation: false, error_message: "" };
+	},
+	methods: {
+		update(current_password) {
+            if (this.new_password == current_password) {
+                this.error_message = "New password must be different";
+                return;
+            }
+
+			update_user_password(
+				this.new_password,
+				current_password,
+				this.server_url
+			)
+				.then((response) => {
+					alert("Password updated successfully!");
+					this.$router.push({ name: "profile" });
+				})
+				.catch(
+					(error) =>
+						(this.error_message = "Incorrect current password")
+				);
+		},
+	},
+};
+</script>
