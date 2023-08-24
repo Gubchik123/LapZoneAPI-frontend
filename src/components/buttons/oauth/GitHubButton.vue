dd90886ecbfc8bae7d49b7ad33af377228ff4d12
Nikita <nikita.gubarev31@gmail.com>
2023-08-24 10:31:27 +0300

    Create components/buttons/oauth/GitHubButton.vue

---
+++ src/components/buttons/oauth/GitHubButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <a
+        @click.prevent="signin"
+        href="#"
+        title="GitHub"
+        class="btn btn-dark w-50 d-flex align-items-center github"
+    >
+        <ion-icon v-pre name="logo-github"></ion-icon>
+        <span class="me-auto ms-auto">GitHub</span>
+    </a>
+</template>
+
+<script>
+import BackendMixin from "@/mixins/BackendMixin";
+
+export default {
+	name: "GitHubButton",
+	mixins: [BackendMixin],
+	methods: {
+		signin() {
+			alert("Soon")
+		},
+	},
+};
+</script>
