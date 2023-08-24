8b7c8dcacc23d091934a9a01daf4bd48b570b9e0
Nikita <nikita.gubarev31@gmail.com>
2023-08-24 14:53:29 +0300

    Create layouts/ProfileLayout.vue

---
+++ src/layouts/ProfileLayout.vue
@@ -0,0 +1,130 @@
+<template>
+	<BaseLayout :title="title" :content_title="title">
+		<template #to_right_of_content_title>
+			<button @click.prevent="sign_out" class="btn btn-warning">
+				Sign out
+			</button>
+		</template>
+		<template #advanced_content>
+			<div class="row px-3 d-flex justify-content-between">
+				<!-- Block with links to other pages -->
+				<div class="col-lg-3 col-md-12 p-0">
+					<div class="list-group">
+						<router-link
+							:to="{ name: 'profile' }"
+							:class="{
+								'active disabled': $route.name == 'profile',
+							}"
+							class="list-group-item list-group-item-action"
+						>
+							Personal details
+						</router-link>
+						<!-- TODO: router-link with :to="{ name: 'update-username' }" -->
+						<a
+							href="#"
+							:class="{
+								'active disabled':
+									$route.name == 'update-username',
+							}"
+							class="list-group-item list-group-item-action"
+						>
+							Change username
+						</a>
+						<!-- TODO: router-link with :to="{ name: 'update-password' }" -->
+						<a
+							href="#"
+							:class="{
+								'active disabled':
+									$route.name == 'update-password',
+							}"
+							class="list-group-item list-group-item-action"
+						>
+							Change password
+						</a>
+						<!-- TODO: router-link with :to="{ name: 'wishes' }" -->
+						<a
+							href="#"
+							:class="{
+								'active disabled': $route.name == 'wishes',
+							}"
+							class="list-group-item list-group-item-action content-between"
+						>
+							Wish list
+						</a>
+						<!-- TODO: router-link with :to="{ name: 'orders' }" -->
+						<a
+							href="#"
+							:class="{
+								'active disabled': $route.name == 'orders',
+							}"
+							class="list-group-item list-group-item-action content-between"
+						>
+							Order list
+						</a>
+					</div>
+				</div>
+
+				<slot></slot>
+			</div>
+
+			<slot name="adding_content"></slot>
+		</template>
+	</BaseLayout>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+
+import BaseLayout from "@/layouts/BaseLayout.vue";
+import BackendMixin from "@/mixins/BackendMixin.js";
+
+import { signout } from "@/api/auth.js";
+
+export default {
+	name: "ProfileLayout",
+	mixins: [BackendMixin],
+	components: { BaseLayout },
+	props: {
+		title: { type: String, required: true },
+	},
+	created() {
+        if (this.user == null) this.get_user();
+	},
+	computed: {
+		...mapGetters("user", ["user"]),
+	},
+	methods: {
+		...mapActions("user", ["get_user"]),
+		sign_out() {
+			signout(this.server_url).then((response) => {
+				localStorage.removeItem("token");
+				this.$router.push({ name: "home" });
+			});
+		},
+	},
+};
+</script>
+
+<style>
+#content {
+	margin-right: 0;
+	margin-left: 20px;
+}
+
+@media screen and (max-width: 992px) {
+	/* Bootstrap5 .lg breakpoint */
+	#content {
+		margin-left: 0;
+	}
+}
+
+@media screen and (max-width: 576px) {
+	/* Bootstrap5 .sm breakpoint */
+	.submit-btn {
+		display: flex !important;
+	}
+	main .w-50 {
+		width: 100% !important;
+	}
+}
+</style>
