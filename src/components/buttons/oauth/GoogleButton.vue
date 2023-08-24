0e517761638ccf6ca0629ed8449ba1faa2c2e19c
Nikita <nikita.gubarev31@gmail.com>
2023-08-24 10:31:13 +0300

    Create components/buttons/oauth/GoogleButton.vue

---
+++ src/components/buttons/oauth/GoogleButton.vue
@@ -0,0 +1,25 @@
+<template>
+	<a
+		@click.prevent="signin"
+		href="#"
+		title="Google"
+		class="btn btn-dark w-50 d-flex align-items-center google"
+	>
+		<ion-icon v-pre name="logo-google"></ion-icon>
+		<span class="me-auto ms-auto">Google</span>
+	</a>
+</template>
+
+<script>
+import BackendMixin from "@/mixins/BackendMixin";
+
+export default {
+	name: "GoogleButton",
+	mixins: [BackendMixin],
+	methods: {
+		signin() {
+			alert("Soon")
+		},
+	},
+};
+</script>
